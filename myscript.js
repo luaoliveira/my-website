ee6485e3728205d25b625e8c58245d4a82f8fdc2
Luana Oliveira <luanaos2512@gmail.com>
2022-06-26 23:31:47 -0300

    First commit

---
+++ myscript.js
@@ -0,0 +1,43 @@
+const targetDiv1=document.getElementById("xpText");
+const targetDiv2=document.getElementById("edText");
+const targetDiv3=document.getElementById("skText");
+const targetDiv4=document.getElementById("lgText");
+
+const names=["xpText", "edText", "skText", "lgText"];
+
+function show(botao){
+    
+    for(const n of names){
+        
+        if(botao.name==n){
+            document.getElementById(n).style.display = 'block';
+        }else{
+            
+            document.getElementById(n).style.display = 'none';
+        }
+        
+    }
+
+}
+
+function validate(){
+
+    const emailFormat= /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    if(document.formulario.Titulo.value==""
+    || document.formulario.Mensagem.value==""
+    || document.formulario.Contato.value==""){
+        alert("Preencha todos os campos do formulário")
+        return false;
+    }
+    else{
+        if(document.formulario.Contato.value.match(emailFormat)){
+            return true;
+        }
+        else{
+            alert("Email inválido")
+            return false;
+        }
+            
+    }
+}
